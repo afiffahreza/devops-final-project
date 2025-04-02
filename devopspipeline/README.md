0aa152625fd89fac53bb48f426bc3b81eafbfdb8
Matt <mho3@andrew.cmu.edu>
2025-04-02 01:52:00 +0000

    Run mvn build and test, logs to S3

--- devopspipeline/README.md
+++ devopspipeline/README.md
@@ -4,22 +4,31 @@ Put github creds into .devcontainer/.env to auto load them into the devcontainer
 
 Create AWS account
 Create IAM [DevOpsPipelineGroup] Group with [ECS+S3Access] policies:
+
 {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "VisualEditor0",
-            "Effect": "Allow",
-            "Action": "ec2:*",
-            "Resource": "*"
-        },
-        {
-            "Sid": "VisualEditor1",
-            "Effect": "Allow",
-            "Action": "s3:*",
-            "Resource": "*"
-        }
-    ]
+	"Version": "2012-10-17",
+	"Statement": [
+		{
+			"Sid": "VisualEditor0",
+			"Effect": "Allow",
+			"Action": "ec2:*",
+			"Resource": "*"
+		},
+		{
+			"Sid": "VisualEditor1",
+			"Effect": "Allow",
+			"Action": [
+				"iam:*"
+			],
+			"Resource": "*"
+		},
+		{
+			"Sid": "VisualEditor2",
+			"Effect": "Allow",
+			"Action": "s3:*",
+			"Resource": "*"
+		}
+	]
 }
 
 Create IAM User [DevOpsPipeline] under the role.
--- devopspipeline/terraform/pipeline.tf
+++ devopspipeline/terraform/pipeline.tf
@@ -52,10 +52,56 @@ resource "aws_security_group" "pipeline_sg" {
   }
 }
 
+resource "aws_iam_role" "pipeline_ec2_role" {
+  name = "pipeline-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "s3_access" {
+  name        = "S3-Access"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:PutObject",
+          "s3:PutObjectAcl"
+        ],
+        Resource = "arn:aws:s3:::17636-devsecops-g2loggingbucket/*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "s3_access_to_pipeline" {
+  role       = aws_iam_role.pipeline_ec2_role.name
+  policy_arn = aws_iam_policy.s3_access.arn
+}
+
+resource "aws_iam_instance_profile" "pipeline_profile" {
+  name = "pipeline-instance-profile"
+  role = aws_iam_role.pipeline_ec2_role.name
+}
+
 resource "aws_instance" "pipeline" {
   ami           = "ami-0efc43a4067fe9a3e"
   instance_type = "t2.small"
   vpc_security_group_ids = [aws_security_group.pipeline_sg.id]
+  iam_instance_profile        = aws_iam_instance_profile.pipeline_profile.name
 
   user_data = templatefile("${path.module}/user_data.sh", {
     GH_DEPLOY_KEY = var.GH_DEPLOY_KEY,
